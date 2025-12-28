a461f3dbd393c4b6c8b2fe717af564e8d45df2ae
DefiRules <cryptochatbox@protonmail.com>
2025-12-27 22:20:25 -0400

    Update Type A apartment prices to ,000

--- components/ApartmentTypesSection.tsx
+++ components/ApartmentTypesSection.tsx
@@ -7,7 +7,7 @@ const apartmentTypes: ApartmentType[] = [
   {
     type: 'A',
     size: '165 m²',
-    price: 'Desde US$406,000',
+    price: 'Desde US$416,000',
     features: [
       '3 habitaciones',
       '3.5 baños',
